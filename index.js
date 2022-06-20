2eb428d54ecf4947f6c4d15080d8349350b5a157
xNotKing <87067415+xNotKing@users.noreply.github.com>
2022-06-20 17:55:02 +0000

    Automatically redirect users to main page.

--- index.js
+++ index.js
@@ -1,1 +1,2 @@
-const Blogs={fetch:()=>{$("#blogs").load("blogs.asp")}};Blogs.fetch();
+location.href="https://xnotking.github.io/"
+//const Blogs={fetch:()=>{$("#blogs").load("blogs.asp")}}Blogs.fetch()
